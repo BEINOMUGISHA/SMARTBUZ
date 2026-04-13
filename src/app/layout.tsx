7b6a49f808cb56e455dffd86efc67561578fe7bd
Atibway <ainebyoonaatiidu@gmail.com>
2026-04-13 18:52:06 +0300

    feat: initialize root layout with global providers and metadata configuration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,6 @@ export const metadata: Metadata = {
 import { ConvexClientProvider } from "@/components/convex-client-provider";
 import { AuthProvider } from "@/context/auth-context";
 import { Toaster } from "@/components/ui/sonner";
-import PaymentWall from "@/components/PaymentWall";
 
 export default function RootLayout({
   children,
@@ -32,14 +31,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <PaymentWall>
-          <ConvexClientProvider>
-            <AuthProvider>
-              {children}
-              <Toaster />
-            </AuthProvider>
-          </ConvexClientProvider>
-        </PaymentWall>
+        <ConvexClientProvider>
+          <AuthProvider>
+            {children}
+            <Toaster />
+          </AuthProvider>
+        </ConvexClientProvider>
       </body>
     </html>
   );
